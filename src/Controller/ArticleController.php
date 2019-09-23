1b615def11caa6926a4f00446df4504fd4f7c5ff
Mozart Rezende <mozart.rezende@memt.local>
2019-09-23 12:25:32 -0300

    Algum progresso foi feito no estudo

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,12 +2,27 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 class ArticleController
 {
+    /**
+     * @Route("/")
+     */
     public function homepage()
     {
         return new Response('OMG! My first page already! WOOO!');
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+        return new Response(sprintf(
+            'Future page to show the article: "%s"',
+            $slug
+        ));
     }
 }
